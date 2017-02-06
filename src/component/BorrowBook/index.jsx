aff412acb1861ae03766a988ecde17cfe7078b4d
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-02-06 15:16:54 +0800

    借书第一步信息填写页面

--- src/component/BorrowBook/index.jsx
+++ src/component/BorrowBook/index.jsx
@@ -23,11 +23,33 @@ import {
 	Toptips
 } from 'react-weui';
 
+import './index.scss';
+
 export default class BorrowBook extends React.Component {
+	changeXiaoQu(value) {
+		console.log(value);
+	}
 	render() {
 		return (
 			<div>
+				<CellsTitle className="cell-title">第一步：信息填写</CellsTitle>
+				<Form radio>
+					<CellsTitle>校区信息</CellsTitle>
+					<FormCell radio onClick={() => this.changeXiaoQu('pf')}>
+							<CellBody>屏峰校区</CellBody>
+							<CellFooter>
+									<Radio name="radio" value="1" defaultChecked/>
+							</CellFooter>
+					</FormCell>
+					<FormCell radio onClick={() => this.changeXiaoQu('zh')}>
+							<CellBody>朝晖校区</CellBody>
+							<CellFooter>
+									<Radio name="radio" value="2"/>
+							</CellFooter>
+					</FormCell>
+				</Form>
 				<Form>
+					<CellsTitle>个人信息</CellsTitle>
 					<FormCell>
 							<CellHeader>
 								<Label>姓名</Label>
@@ -49,10 +71,19 @@ export default class BorrowBook extends React.Component {
 								<Label>学号</Label>
 							</CellHeader>
 							<CellBody>
-									<Input type="text" placeholder="你的学号"/>
+									<Input type="number" placeholder="你的学号"/>
 							</CellBody>
 					</FormCell>
-				</Form>	
+					<FormCell>
+							<CellHeader>
+								<Label>联系方式</Label>
+							</CellHeader>
+							<CellBody>
+									<Input type="text" placeholder="长短号"/>
+							</CellBody>
+					</FormCell>
+				</Form>
+				<Button className="large-btn">下一步</Button>
 			</div>
 		)
 	}
