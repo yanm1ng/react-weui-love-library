6cf68c5d18a55181ff0cdf575ff45deae155d242
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-02-06 16:23:40 +0800

    借书第一步信息填写页面逻辑

--- src/component/BorrowBook/index.jsx
+++ src/component/BorrowBook/index.jsx
@@ -26,64 +26,108 @@ import {
 import './index.scss';
 
 export default class BorrowBook extends React.Component {
-	changeXiaoQu(value) {
-		console.log(value);
+	constructor(props) {
+		super(props);  
+    this.state = {
+			step: 0,
+			form: {
+				xiaoqu: '屏峰校区',
+				name: '',
+				xueyuan: '',
+				xuehao: '',
+				phone: ''
+			}
+    };
+  }
+	changeRadio(value) {
+		const {
+			form
+		} = this.state;
+		form.xiaoqu = value;
+		this.setState({
+			form
+		});
+	}
+	handleStep(step) {
+		const {
+			form
+		} = this.state;
+
+		if(step === 1) {
+			const arr = ['name', 'xueyuan', 'xuehao', 'phone'];
+			arr.forEach(index => {
+				form[index] = document.getElementById(index).value.trim();
+			});
+			this.setState({
+				form,
+				step
+			});
+		}
 	}
 	render() {
 		return (
 			<div>
-				<CellsTitle className="cell-title">第一步：信息填写</CellsTitle>
-				<Form radio>
-					<CellsTitle>校区信息</CellsTitle>
-					<FormCell radio onClick={() => this.changeXiaoQu('pf')}>
-							<CellBody>屏峰校区</CellBody>
-							<CellFooter>
-									<Radio name="radio" value="1" defaultChecked/>
-							</CellFooter>
-					</FormCell>
-					<FormCell radio onClick={() => this.changeXiaoQu('zh')}>
-							<CellBody>朝晖校区</CellBody>
-							<CellFooter>
-									<Radio name="radio" value="2"/>
-							</CellFooter>
-					</FormCell>
-				</Form>
-				<Form>
-					<CellsTitle>个人信息</CellsTitle>
-					<FormCell>
-							<CellHeader>
-								<Label>姓名</Label>
-							</CellHeader>
-							<CellBody>
-									<Input type="text" placeholder="你的姓名"/>
-							</CellBody>
-					</FormCell>
-					<FormCell>
-							<CellHeader>
-								<Label>学院</Label>
-							</CellHeader>
-							<CellBody>
-									<Input type="text" placeholder="你的学院"/>
-							</CellBody>
-					</FormCell>
-					<FormCell>
-							<CellHeader>
-								<Label>学号</Label>
-							</CellHeader>
-							<CellBody>
-									<Input type="number" placeholder="你的学号"/>
-							</CellBody>
-					</FormCell>
-					<FormCell>
-							<CellHeader>
-								<Label>联系方式</Label>
-							</CellHeader>
-							<CellBody>
-									<Input type="text" placeholder="长短号"/>
-							</CellBody>
-					</FormCell>
-				</Form>
-				<Button className="large-btn">下一步</Button>
+				{	this.state.step === 0 &&
+					<div>
+						<CellsTitle className="cell-title">第一步：信息填写</CellsTitle>
+						<Form radio>
+							<CellsTitle>校区信息</CellsTitle>
+							<FormCell radio onClick={() => this.changeRadio('屏峰校区')}>
+									<CellBody>屏峰校区</CellBody>
+									<CellFooter>
+											<Radio name="radio" value="1" defaultChecked/>
+									</CellFooter>
+							</FormCell>
+							<FormCell radio onClick={() => this.changeRadio('朝晖校区')}>
+									<CellBody>朝晖校区</CellBody>
+									<CellFooter>
+											<Radio name="radio" value="2"/>
+									</CellFooter>
+							</FormCell>
+						</Form>
+						<Form>
+							<CellsTitle>个人信息</CellsTitle>
+							<FormCell>
+									<CellHeader>
+										<Label>姓名</Label>
+									</CellHeader>
+									<CellBody>
+											<Input type="text" placeholder="你的姓名" id="name"/>
+									</CellBody>
+							</FormCell>
+							<FormCell>
+									<CellHeader>
+										<Label>学院</Label>
+									</CellHeader>
+									<CellBody>
+											<Input type="text" placeholder="你的学院" id="xueyuan"/>
+									</CellBody>
+							</FormCell>
+							<FormCell>
+									<CellHeader>
+										<Label>学号</Label>
+									</CellHeader>
+									<CellBody>
+											<Input type="number" placeholder="你的学号" id="xuehao"/>
+									</CellBody>
+							</FormCell>
+							<FormCell>
+									<CellHeader>
+										<Label>联系方式</Label>
+									</CellHeader>
+									<CellBody>
+											<Input type="text" placeholder="长短号" id="phone"/>
+									</CellBody>
+							</FormCell>
+						</Form>
+						<Button className="large-btn" onClick={() => this.handleStep(1)}>下一步</Button>
+					</div>
+				}
+				{	this.state.step === 1 &&
+					<div>
+						<CellsTitle className="cell-title">{this.state.form.xiaoqu + ' >> ' + this.state.form.name}</CellsTitle>
+					</div>
+				}
 			</div>
 		)
 	}
