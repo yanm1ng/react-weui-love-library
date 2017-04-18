5fb8462ac277062c41ee87919ecaa0191cdce1d1
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-04-18 17:33:48 +0800

    express with webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,26 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: ['webpack/hot/dev-server', path.resolve(__dirname, './src/router/index')],
+	entry: [
+		'webpack-hot-middleware/client',
+		path.resolve(__dirname, './src/router/index'),
+	],
 	output: {
 		path: path.resolve(__dirname, './build'),
-		filename: 'bundle.js'
-	},
-	devServer: {
-		contentBase: '',
-		devtool: 'eval',
-		hot: true,
-		inline: true,
-		port: 8181
+		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	module: {
 		loaders: [
 			{
 				test: /\.(js|jsx)$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					presets: ['react', 'es2015'],
-				}
+				loaders: ['react-hot', 'babel?presets[]=es2015,presets[]=react'],
+				exclude: path.resolve(__dirname, 'node_modules'),
 			}, {
 				test: /\.(css|scss)$/,
 				loader: ExtractTextPlugin.extract("style", "css!sass!autoprefixer")
 			}, {
-				test: /\.(png|jpg|jpeg|gif|eot|woff|svg|ttf|woff2|appcache)(\?|$)/,
-				exclude: /^node_modules$/,
+				test: /\.(png|jpg|jpeg|gif|eot|woff|svg|ttf|woff2)(\?|$)/,
 				loader: 'file-loader?limit=80000&name=[name].[ext]'
 			}
 		]
@@ -40,7 +33,12 @@ module.exports = {
 		extensions: ['', '.js', '.jsx']
 	},
 	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+  	new webpack.NoErrorsPlugin(),
 		new webpack.optimize.UglifyJsPlugin({
+			output: {
+        comments: false,
+      },
 			compressor: {
 				warnings: false
 			}
